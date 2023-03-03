c8042096ea907ec3c8efdb4184c72cc0c44375b1
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-03 21:35:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ function register(event) {
     var userPhone = document.getElementById("userPhone").value;
     var userEmail = document.getElementById("userEmail").value;
     var userPassword = document.getElementById("userPassword").value;
-    var userData = { name: userName, number : userPhone, email: userEmail, password: userPassword }
+    var userData = { name: userName, number: userPhone, email: userEmail, password: userPassword }
 
     // storing data from js to ls
     var dataFromLS = JSON.parse(localStorage.getItem("userData")) || [];
@@ -29,8 +29,35 @@ function register(event) {
         document.getElementById("userPhone").value = " ";
         document.getElementById("userEmail").value = " ";
         document.getElementById("userPassword").value = " ";
-        window.location.href = "/amazonhome.html";
+        window.location.href = "/amazonlogin.html";
         alert("Registration done");
+       
+    }
+}
 
+function login(event) {
+    event.preventDefault();
+    var userEmail = document.getElementById("email").value;
+    var userPassword = document.getElementById("password").value;
+    var userData = { email: userEmail, password: userPassword }
+
+    var dataFromLS = JSON.parse(localStorage.getItem("userData")) || [];
+    console.log(dataFromLS, 'dataFromLS')
+    var flag = false;
+    for (var i = 0; i < dataFromLS.length; i++) {
+        if (dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword) {
+            flag = true;
+        }
+    }
+    if (flag === true) {
+        dataFromLS.push(userData);
+        localStorage.setItem("userData", JSON.stringify(dataFromLS));
+        document.getElementById("email").value = " ";
+        document.getElementById("password").value = " ";
+        window.location.href = '/amazonhome.html';
+        alert("login succecfully")
+    } else {
+
+        alert("Wrong cred, Please check your email and password");
     }
 }
